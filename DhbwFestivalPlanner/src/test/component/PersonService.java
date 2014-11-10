1ae5274b4b594d636cba29fafecccc7b20205a84
therealzteve <htczteve@gmail.com>
2014-11-10 10:08:06 +0100

    Hello World MVC with Spring

---
+++ DhbwFestivalPlanner/src/test/component/PersonService.java
@@ -0,0 +1,8 @@
+package test.component;
+
+public class PersonService implements IPersonService {
+	@Override
+	public String getPersonName() {
+		return "Ram";
+	}
+}
---
+++ DhbwFestivalPlanner/src/test/hello/MessageService.java
@@ -0,0 +1,5 @@
+package test.hello;
+
+public interface MessageService {
+	public String getMessage();
+}
