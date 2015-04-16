e4a83ad2b9f269a132a371d1bc2773f20296f344
ytalia1994 <ytalia94@yahoo.de>
2015-04-16 08:49:59 +0200

    updated Unit Test for RandomString.java

--- DhbwFestivalPlanner/src/com/helper/RandomStringTest.java
+++ DhbwFestivalPlanner/src/com/helper/RandomStringTest.java
@@ -1,19 +1,33 @@
 package com.helper;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RandomStringTest {
+	int length;
+	RandomString s1,s2;
+	
+	@Before
+	public void setup(){
+		length=5;
+		s1= new RandomString(length);
+		s2= new RandomString(length);
+	}
 
 	@Test
 	public void test() {
-		int length=5;
-		RandomString s1= new RandomString(length);
-		RandomString s2= new RandomString(length);
 		assertEquals(s1.resultStringLength(), length);
 		assertEquals(s2.resultStringLength(), length);
 		assertNotSame(s1.resultString(), s2.resultString());
+	}
+	
+	@After
+	public void end(){
+		System.out.println("Test Completed");
 	}
 
 }
