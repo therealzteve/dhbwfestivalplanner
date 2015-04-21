db09fff749f38d7bf2b5d655072b96cc93b98e68
ytalia1994 <ytalia94@yahoo.de>
2015-04-21 13:47:34 +0200

    FEST-34: Email validation

--- DhbwFestivalPlanner/WebContent/views/js/handler.js
+++ DhbwFestivalPlanner/WebContent/views/js/handler.js
@@ -98,4 +98,18 @@ $('#time').datetimepicker({
 	  format:'H:i'
 	});}
 
+function validateEmail(id) 
+{//return false;
+	var email = document.getElementById(id);
+	console.log(email.value);
+	//alert("defgtzhujkl");
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!filter.test(email.value)) {
+        alert('Please provide a valid email address');
+        //email.focus;
+        return false;
+}}
+
 $(document).ready(function(){pickDate();pickTime();})
+
+
