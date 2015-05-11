d67104264f7fd8e47daada14e11b45e71540dc3d
Steven Wruk <htczteve@gmail.com>
2015-05-11 15:18:11 +0200

    - Fixed Spring Configuration

--- DhbwFestivalPlanner/src/com/config/security/SecurityConfig.java
+++ DhbwFestivalPlanner/src/com/config/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
